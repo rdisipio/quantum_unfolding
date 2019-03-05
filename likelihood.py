cc596d35c7320e663f2ec4285d771b865482bbd1
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-03-05 14:28:27 -0500

    unfolding w/out reg

--- likelihood.py
+++ likelihood.py
@@ -3,13 +3,24 @@ import numpy as np
 
 
 def log_gauss(d_b, y_b):
+    d_d = np.packbits(d_b)
+    y_d = np.packbits(y_b)
+    n = d_d.shape[0]
+    z = 0.
+    for k in range(n):
+        s = (int(d_d[k]) - int(y_d[k]))
+        z += s*s
+    return z
+
+
+def test_lhood(d_b, y_b):
     z = np.uint8(0)
     n = d_b.shape[0]
     r = np.zeros(n, dtype='uint8')
 
-    for k in range(n):
+    for k in range(n-1, -1, -1):
         r[k] = ~(d_b[k] ^ y_b[k])
-    for k in range(n):
-        z ^= r[k]
+    # for k in range(n-1, -1, -1):
+        z ^= (r[k] << k)
         #z += abs(d_b[k] - y_b[k])
     return z
--- toy_unfolding_classical.py
+++ toy_unfolding_classical.py
@@ -30,7 +30,8 @@ R = [[5, 1, 0],
      [0, 1, 2]]
 
 # pseudo-data:
-d = [32, 40, 15]
+#d = [32, 40, 15]
+d = np.dot(R, x)
 
 # convert inputs to appropriate format
 
@@ -39,12 +40,14 @@ print(xedges)
 
 x = np.array(x, dtype='uint8')
 x_b = np.unpackbits(x)
-print("INFO: x binary representation:", x_b.shape)
+print("INFO: truth level:")
+print(x)
 print(x_b)
 
 d = np.array(d, dtype='uint8')
 d_b = np.unpackbits(d)
-print("INFO: d binary representation:", d_b.shape)
+print("INFO: pseudo-data:")
+print(d)
 print(d_b)
 
 # Response matrix
@@ -52,34 +55,37 @@ print(d_b)
 R = np.array(R, dtype='uint8')
 
 R_b = d2b(R)
+print("INFO: Response matrix:")
+print(R)
 print("INFO: R binary representation:", R_b.shape)
 print(R_b)
 
 # Now loop to find the minimum of the likelihood
 n_itr = 5000
 k_min = -1
-z_min = 1000000000
-y_hat_best = None
+logL_min = 10000000
+x_hat_bestfit_b = None
 for k in range(n_itr):
-    x_hat = np.random.randint(0, int_max, size=(n_bins), dtype='uint8')
-    x_hat_b = np.unpackbits(x_hat)
-
+    #x_hat = np.random.randint(0, int_max, size=(n_bins), dtype='uint8')
+    #x_hat_b = np.unpackbits(x_hat)
+    x_hat_b = np.random.randint(0, 2, size=(n_bins*n_bits), dtype='uint8')
     y_hat_b = binary_matmul(R_b, x_hat_b)
 
-    z = lh.log_gauss(d_b, y_hat_b)
-    #print(k, z)
+    logL = lh.log_gauss(d_b, y_hat_b)
+    #print(k, logL_min, logL)
 
-    if z > z_min:
+    if logL > logL_min:
         continue
 
-    z_min = z
+    logL_min = logL
     k_min = k
-    y_hat_best_b = np.array(y_hat_b, dtype='uint8')
+    x_hat_bestfit_b = np.array(x_hat_b, dtype='uint8')
 
 print("INFO: logL minimum found at iteration %i:" % k_min)
-print(y_hat_best_b)
-y_hat_best = np.packbits(y_hat_best_b)
-print(y_hat_best)
+x_hat_bestfit = np.packbits(x_hat_bestfit_b)
+print("INFO: bestfit:")
+print(x_hat_bestfit_b)
+print(x_hat_bestfit)
 print("INFO: truth level:")
 print(x_b)
 print(x)
