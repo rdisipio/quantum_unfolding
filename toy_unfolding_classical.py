9954f2ff485cbe13430275050860c73b46075977
Peter Wittek <peterwittek@users.noreply.github.com>
2019-03-19 21:09:28 -0400

    Reduced code complexity

--- toy_unfolding_classical.py
+++ toy_unfolding_classical.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-
 import numpy as np
-import matplotlib.pyplot as plt
-from decimal2binary import *
+from decimal2binary import binary_matmul, d2b, get_int_max
 import likelihood as lh
 
 np.set_printoptions(precision=1, linewidth=200, suppress=True)
 
 # All numbers are 8 bits long.
 # Smaller Numpy dtype is uint8, i.e. [0-255]
-
 
 ######################
 # Set the inputs here
@@ -19,15 +16,14 @@ np.set_printoptions(precision=1, linewidth=200, suppress=True)
 xmin = 0
 xmax = 6
 n_bins = 3
-xedges = np.linspace(xmin, xmax, n_bins+1)  # for uniform binning
+n_bits = 8
+xedges = np.linspace(xmin, xmax, n_bins + 1)  # for uniform binning
 
 # truth-level:
 x = [5, 10, 3]
 
 # response matrix:
-R = [[5, 1, 0],
-     [1, 3, 1],
-     [0, 1, 2]]
+R = [[5, 1, 0], [1, 3, 1], [0, 1, 2]]
 
 # pseudo-data:
 #d = [32, 40, 15]
@@ -38,21 +34,17 @@ d = np.dot(R, x)
 print("INFO: bin edges (%i):" % n_bins)
 print(xedges)
 
-x = np.array(x, dtype='uint8')
-x_b = np.unpackbits(x)
+x_b = d2b(x)
 print("INFO: truth level:")
 print(x)
 print(x_b)
 
-d = np.array(d, dtype='uint8')
-d_b = np.unpackbits(d)
+d_b = d2b(d)
 print("INFO: pseudo-data:")
 print(d)
 print(d_b)
 
 # Response matrix
-
-R = np.array(R, dtype='uint8')
 
 R_b = d2b(R)
 print("INFO: Response matrix:")
@@ -61,25 +53,22 @@ print("INFO: R binary representation:", R_b.shape)
 print(R_b)
 
 # Now loop to find the minimum of the likelihood (brute force approach)
-n_itr = 500000
-k_min = -1
-logL_min = 1000000000000
-x_hat_bestfit_b = None
-for k in range(n_itr):
+n_iterations = 500000
+logL_min = np.finfo('d').max
+for k in range(n_iterations):
     # generate decimal, then convert to binary...
-    x_hat = np.random.randint(0, int_max, size=(n_bins), dtype='uint8')
-    x_hat_b = np.unpackbits(x_hat)
+    x_hat_b = d2b(np.random.randint(0, get_int_max(n_bits), size=(n_bins), dtype='uint8'))
 
     # or generate binary, then convert to decimal
-    #x_hat_b = np.random.randint(0, 2, size=(n_bins*n_bits), dtype='uint8')
+    # x_hat_b = np.random.randint(0, 2, size=(n_bins*n_bits), dtype='uint8')
 
     # apply response to obtain reco-level prediction
     y_hat_b = binary_matmul(R_b, x_hat_b)
 
     # calculate likelihood
     logL = lh.log_gauss(d_b, y_hat_b)
-    #logL, r = lh.test_lhood(d_b, y_hat_b)
-    #print(k, logL_min, logL)
+    # logL, r = lh.test_lhood(d_b, y_hat_b)
+    # print(k, logL_min, logL)
 
     # update minimum
     if logL > logL_min:
