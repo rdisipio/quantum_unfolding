90cb55b2c8cd15bddf4e0f4e0b2c5eee459a7182
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-03-22 11:19:28 -0400

    test discretize algorithm

--- decimal2binary.py
+++ decimal2binary.py
@@ -1,6 +1,31 @@
 import numpy as np
 
 
+def laplacian(n):
+
+    lap = np.diag(2*np.ones(n)) + \
+        np.diag(-1*np.ones(n-1), 1) + \
+        np.diag(-1*np.ones(n-1), -1)
+
+    return lap
+
+
+def laplacian_nbits(N, n=8):
+    A = np.zeros([N, N*n])
+    bitvals = [np.power(2., -i) for i in range(n)]
+    for i in range(n):
+        A[0, i] = -2 * bitvals[i]
+        A[0, n + i] = 1 * bitvals[i]
+        A[-1, - 2 * n + i] = 1 * bitvals[i]
+        A[-1, - n + i] = -2 * bitvals[i]
+    for i in range(1, N-1):
+        for j in range(n):
+            A[i, n * i - 2 * n + j] = 1 * bitvals[j]
+            A[i, n * i - n + j] = -2 * bitvals[j]
+            A[i, n * i + j] = 1 * bitvals[j]
+    return A
+
+
 def get_int_max(n_bits):
     return 2**n_bits - 1
 
@@ -68,3 +93,36 @@ def binary_matmul(A, x):
             y[i] ^= p
             y[i - 1] ^= c
     return y
+
+
+def discretize_vector(x, n=8):
+    N = len(x)
+
+    q = np.zeros(N*n)
+    for i in range(N-1, -1, -1):
+        x_d = int(x[i])
+        j = n-1
+        while x_d > 0:
+            k = i*n + j
+            q[k] = x_d % 2
+            x_d = x_d // 2
+            j -= 1
+    return np.uint8(q)
+
+
+def discretize_matrix(A, n=8):
+    # x has N elements (decimal)
+    # q has Nx elements (binary)
+    # A has N columns
+    # D has Nn columns
+    # Ax = Dq
+
+    N = A.shape[0]
+
+    D = np.zeros([N, N*n])
+
+    for i in range(N):
+        for j in range(n):
+            k = (i)*n+j
+            D[:, k] = np.power(2, n-j-1) * A[:, i]
+    return D
