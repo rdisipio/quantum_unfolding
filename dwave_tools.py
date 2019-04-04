d8b37f4c0bcd1fbc62e5b1fe0838f4a419dcc444
Peter Wittek <peterwittek@users.noreply.github.com>
2019-04-03 20:28:42 -0400

    Minor embedding added

---
+++ dwave_tools.py
@@ -0,0 +1,45 @@
+import dwave_networkx as dnx
+import minorminer
+import numpy as np
+import sys
+from dwave.system.composites import FixedEmbeddingComposite
+from dwave.system.samplers import DWaveSampler
+
+
+def max_chain_length(embedding: dict)->int:
+    max_ = 0
+    for _, chain in embedding.items():
+        if len(chain) > max_:
+            max_ = len(chain)
+    return max_
+
+
+def get_embedding_with_short_chain(J: dict, tries=5, verbose=False)->dict:
+    '''Try a few probabilistic embeddings and return the one with the shortest
+    chain length
+
+    :param J: Couplings
+    :param tries: Number of probabilistic embeddings
+    :param verbose: Whether to print out diagnostic information
+
+    :return: Returns the minor embedding
+    '''
+    # The hardware topology: 16 by 16 pieces of K_4,4 unit cells
+    processor = dnx.chimera_graph(16, 16, 4)
+    # Try a few embeddings
+    best_chain_length = sys.maxsize
+    source = list(J.keys())
+    for _ in range(tries):
+        try:
+            emb = minorminer.find_embedding(source, processor.edges())
+            chain_length = max_chain_length(emb)
+            if chain_length > 0 and chain_length < best_chain_length:
+                embedding = emb
+                best_chain_length = chain_length
+        except:
+            pass
+    if verbose:
+        print(best_chain_length, max_chain_length(embedding))
+    if best_chain_length == sys.maxsize:
+        raise Exception("Cannot find embedding")
+    return embedding
---
+++ unfolding_qubo.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import argparse
+import numpy as np
+from decimal2binary import *
+import dimod
+from dwave.system import FixedEmbeddingComposite, DWaveSampler
+from dwave_tools import get_embedding_with_short_chain
+
+np.set_printoptions(precision=1, linewidth=200, suppress=True)
+
+parser = argparse.ArgumentParser("Quantum unfolding")
+parser.add_argument('-l', '--lmbd', default=0.00)
+parser.add_argument('-n', '--nreads', default=10000)
+parser.add_argument('-b', '--backend', default='cpu')
+args = parser.parse_args()
+
+num_reads = int(args.nreads)
+
+# truth-level:
+x = [5, 10, 3]
+
+# response matrix:
+R = [[3, 1, 0], [1, 3, 1], [0, 1, 2]]
+
+# pseudo-data:
+d = [32, 40, 15]
+
+# convert to numpy arrays
+x = np.array(x, dtype='uint8')
+R = np.array(R, dtype='uint8')
+b = np.array(d, dtype='uint8')
+
+# closure test
+b = np.dot(R, x)
+
+n = 4
+N = x.shape[0]
+
+print("INFO: N bins:", N)
+print("INFO: n-bits encoding:", n)
+
+lmbd = np.uint8(args.lmbd)  # regularization strength
+D = laplacian(N)
+
+# convert to bits
+x_b = discretize_vector(x, n)
+b_b = discretize_vector(b, n)
+R_b = discretize_matrix(R, n)
+D_b = discretize_matrix(D, n)
+
+print("INFO: Truth-level x:")
+print(x, x_b)
+print("INFO: pseudo-data b:")
+print(b, b_b)
+print("INFO: Response matrix:")
+print(R)
+print(R_b)
+print("INFO: Laplacian operator:")
+print(D)
+print(D_b)
+print("INFO: regularization strength:", lmbd)
+
+# Create QUBO operator
+
+# linear constraints
+h = {}
+for j in range(n*N):
+    idx = (j)
+    h[idx] = 0
+    for i in range(N):
+        h[idx] += (R_b[i][j]*R_b[i][j] -
+                   2*R_b[i][j] * b[i] +
+                   lmbd*D_b[i][j]*D_b[i][j])
+    #print("h", idx, ":", h[idx])
+
+# quadratic constraints
+J = {}
+for j in range(n*N):
+    for k in range(j+1, n*N):
+        idx = (j, k)
+        J[idx] = 0
+        for i in range(N):
+            J[idx] += 2*(R_b[i][j]*R_b[i][k] + lmbd*D_b[i][j]*D_b[i][k])
+        #print("J", idx, ":", J[idx])
+
+# QUBO
+bqm = dimod.BinaryQuadraticModel(linear=h,
+                                 quadratic=J,
+                                 offset=0.0,
+                                 vartype=dimod.BINARY)
+print("INFO: solving the QUBO model...")
+
+result = None
+if args.backend == 'cpu':
+    print("INFO: running on CPU...")
+    result = dimod.ExactSolver().sample(bqm)
+elif args.backend == 'qpu':
+    print("INFO: running on QPU...")
+    embedding = get_embedding_with_short_chain(J)
+    sampler = FixedEmbeddingComposite(DWaveSampler(), embedding)
+    result = sampler.sample(bqm, num_reads=num_reads).aggregate()
+print("INFO: ...done.")
+
+result = result.first
+energy = result.energy
+q = np.array(list(result.sample.values()))
+y = compact_vector(q, n)
+print("INFO: best-fit:   ", q, "::", y, ":: E =", energy)
+print("INFO: truth value:", x_b, "::", x)
